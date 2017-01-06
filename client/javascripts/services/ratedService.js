b06a73c2da9580ed5cb83a4568607878d686bc0d
Matt Gardner <matthewlgardner@gmail.com>
2017-01-06 11:05:59 -0700

    add db to heroku

--- client/javascripts/services/ratedService.js
+++
@@ -1,9 +0,0 @@
-app.service('ratedService', function($http) {
-    // console.log('i got to rated service.js')
-    return {
-        top10: function() {
-          // console.log('something got returned');
-            return $http.get('/api/top10')
-        }
-    }
-})
