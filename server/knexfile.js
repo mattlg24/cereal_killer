b85a0ca23c57fa56c600e50458ab0d87f3b0dcd2
Matt Gardner <matthewlgardner@gmail.com>
2017-01-06 10:06:38 -0700

    add production to knexfile.js

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,17 +5,8 @@ module.exports = {
         client: 'pg',
         connection: 'postgres://localhost/cereal_killer'
     },
-    // module.exports = {
-    //     development: {
-    //         client: 'mysql',
-    //         connection: {
-    //             user: 'matt',
-    //             password: 'some_pass',
-    //             database: 'cereal_killer'
-    //         }
-    //     },
-    // production: {
-    //     client: 'mysql',
-    //     connection: process.env.DATABASE_URL + '?ssl=true'
-    // }
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
+    }
 };
